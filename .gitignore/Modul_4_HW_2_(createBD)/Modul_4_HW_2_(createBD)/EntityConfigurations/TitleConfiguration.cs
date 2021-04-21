eedb3e3cb40aa3de00edbfa059702189a0c6653e
VavriichukDima <78602077+VavriichukDima@users.noreply.github.com>
2021-04-21 14:37:20 +0300

    Project

---
+++ .gitignore/Modul_4_HW_2_(createBD)/Modul_4_HW_2_(createBD)/EntityConfigurations/TitleConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Modul_4_HW_2__createBD_.Entities;
+
+namespace Modul_4_HW_2__createBD_.EntityConfigurations
+{
+    public class TitleConfiguration : IEntityTypeConfiguration<Title>
+    {
+        public void Configure(EntityTypeBuilder<Title> builder)
+        {
+            builder.ToTable("Title").HasKey(p => p.TitleId);
+            builder.Property(p => p.TitleId).HasColumnName("TitleId").IsRequired();
+            builder.Property(p => p.Name).HasColumnName("Name").HasMaxLength(50).IsRequired();
+
+            builder.HasMany(c => c.Employees)
+                .WithOne(w => w.Title)
+                .HasForeignKey(f => f.EmployeeId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
